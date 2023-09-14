6a26c920703b5159190d72ecb857c11afb5eb19c
kyle <kaplaugher94@gmail.com>
2023-09-13 20:15:46 -0400

    add search bar

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <script>
-
+	import { Search, Button } from 'flowbite-svelte';
 </script>
 
 <svelte:head>
@@ -7,11 +7,14 @@
 	<meta name="description" content="Svelte demo app" />
 </svelte:head>
 
-<section>
-	<h1 class="text-blue-500">Hello there</h1>
-
-	
-
-
+<section class="h-screen w-screen">
+	<div
+		class="h-full w-full bg-cover bg-no-repeat mx-auto flex flex-col justify-center items-center"
+		style=" background-image: url('https://ggheaven.com/wp-content/uploads/2022/04/World-Of-Warships.jpg);"
+	>
+		<form class="flex gap-2">
+			<Search size="md" />
+			<Button class="!p-2.5">Search</Button>
+		</form>
+	</div>
 </section>
-
